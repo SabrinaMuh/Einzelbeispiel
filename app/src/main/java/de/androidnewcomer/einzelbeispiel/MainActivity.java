4b269321ad4ee63ed51730b5229c86fb153ff244
Sabrina Muhrer <sabrinamuh@edu.aau.at>
2021-03-07 18:17:51 +0100

    Aufgabe1

--- app/src/main/java/de/androidnewcomer/einzelbeispiel/MainActivity.java
+++ app/src/main/java/de/androidnewcomer/einzelbeispiel/MainActivity.java
@@ -1,23 +1,16 @@
 package de.androidnewcomer.einzelbeispiel;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.*;
 import java.net.*;
 
-public class MainActivity extends Activity implements android.view.View.OnClickListener {
+public class MainActivity extends Activity{
 
     TextView textViewAnswer;
     EditText editText;
@@ -26,25 +19,55 @@ public class MainActivity extends Activity implements android.view.View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
+        Button buttonAbschicken = (Button) findViewById(R.id.buttonAbschicken);
+        final Button buttonBerechnen = (Button) findViewById(R.id.buttonBerechnung);
         textViewAnswer = (TextView) findViewById(R.id.textViewServerAntwort);
         editText = (EditText) findViewById(R.id.editText);
-        button.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View v){
-        String matrikelnr = editText.getText().toString();
-        SimpleThread t = new SimpleThread(matrikelnr);
+        final String matrikelnr = editText.getText().toString();
+        buttonAbschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                SimpleThread t = new SimpleThread(matrikelnr);
 
-        t.start();
+                t.start();
 
-        try {
-            t.join();
-        }catch (InterruptedException ie){
-            //
-        }
+                try {
+                    t.join();
+                }catch (InterruptedException ie){
+                    //
+                }
 
-        textViewAnswer.setText(t.answer);
+                textViewAnswer.setText(t.answer);
+                if (!t.answer.equals("Dies ist keine gueltige Matrikelnummer")){
+                buttonBerechnen.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        buttonBerechnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textviewBerechnung = (TextView) findViewById(R.id.textViewBerechnung);
+                char[] matrikelarray = new char[matrikelnr.length()];
+                char[] character = new char[11];
+
+                for (int i = 0; i < matrikelarray.length; i++) {
+                    matrikelarray[i] = matrikelnr.charAt(i);
+                }
+                char i = 'a';
+                character[0] = '0';
+                for (int j = 1; j < character.length; j++) {
+                    character[j] = i;
+                    i++;
+                }
+                for (int j = 1; j < matrikelarray.length; j=j+2) {
+                    for (int k = 0; k < character.length; k++) {
+                        if (k == matrikelarray[j]) matrikelarray[j] = character[k];
+                    }
+                }
+                String newmatrikelnr = new String(matrikelarray);
+                textviewBerechnung.setText(newmatrikelnr);
+            }
+        });
     }
 }
 
