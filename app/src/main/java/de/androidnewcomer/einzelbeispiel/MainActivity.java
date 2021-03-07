7f71fb976b7585640d15f7f2926027a2ccfbd8c9
Sabrina Muhrer <sabrinamuh@edu.aau.at>
2021-03-07 13:59:33 +0100

    Server

--- app/src/main/java/de/androidnewcomer/einzelbeispiel/MainActivity.java
+++ app/src/main/java/de/androidnewcomer/einzelbeispiel/MainActivity.java
@@ -2,12 +2,50 @@ package de.androidnewcomer.einzelbeispiel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.*;
+import java.net.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        try {
+            EditText editText = (EditText) findViewById(R.id.editText);
+            String matrikelnr = editText.getText().toString();
+            tcpClient(matrikelnr);
+        }catch (Exception e){
+
+        }
+    }
+
+    protected void tcpClient(String matrikelnr) throws Exception{
+        String sentence;
+        String modifiedSentence;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence = matrikelnr;
+
+        outToServer.writeBytes(sentence +'\n');
+
+        modifiedSentence = inFromServer.readLine();
+
+
     }
 }
